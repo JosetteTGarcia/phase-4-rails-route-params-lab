24aada7c805377c5edb33a3e4b4357c7d99cc9c7
Josette Garcia <josettetgarcia@gmail.com>
2022-07-16 12:36:15 -0500

    commit tree 491b03121816923dc47c071b1e1a2f3b7bbbd563

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,15 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def one_student
+    student = Student.find(params[:id])
+    render json: student
+  end
+
+
+  def students_by_name
+    byebug
+    # student1 = Student.find(params[:name1])
+    # student2 = Student.find(params[:name2])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   get '/students', to: 'students#index'
   get '/students/grades', to: 'students#grades'
   get '/students/highest-grade', to: 'students#highest_grade'
+
+  get '/students/:id', to: 'students#one_student'
+  
+  get '/students?name=:value', to: 'students#sudents_by_name'
 end
